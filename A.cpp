18ce17b2d5c3b1da3c86ddc95f7996f6bbcc595c
Akshat Kaushik <kaushikakshat007@gmail.com>
2019-03-18 10:54:42 +0530

    Question2 Commit One

---
+++ A.cpp
@@ -0,0 +1,18 @@
+ include <iostream>
+using namespace std;
+
+int main()
+{
+    char op;
+    float num1, num2;
+
+    cout << "Enter operator +: ";
+    cin >> op;
+
+    cout << "Enter two operands: ";
+    cin >> num1 >> num2;
+    cout << num1+num2;
+
+    return 0;
+}
+           
