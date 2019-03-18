d3ceeda91894dd7deeac5b472cf5a41a5728734d
Akshat Kaushik <kaushikakshat007@gmail.com>
2019-03-18 11:00:00 +0530

    Question2 Commit Three

--- A.cpp
+++ A.cpp
@@ -6,7 +6,7 @@ int main()
     char op;
     float num1, num2;
 
-    cout << "Enter operator either + or - : ";
+    cout << "Enter operator either + or - or * or /: ";
     cin >> op;
 
     cout << "Enter two operands: ";
@@ -20,6 +20,18 @@ int main()
 
         case '-':
             cout << num1-num2;
+            break;
+
+	case '*':
+            cout << num1*num2;
+            break;
+
+        case '/':
+            cout << num1/num2;
+            break;
+	default:
+            // If the operator is other than +, -, * or /, error message is shown
+            cout << "Error! operator is not correct";
             break;
      }
     return 0;
